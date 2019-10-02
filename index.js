a8d8212b4e00835706e6af6ec76f2da13e9cdd49
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-02 16:49:08 -0500

    first commit set up for files done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+
+const server = require('./server.js');
+
+const port = 9102
+server.listen(port, () => {
+    console.log(`\n** Server listening on ${port}`);
+})
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
+//middleware
+const posts = require('./posts/postRouter.js');
+const users = require('./users/userRouter.js');
+
+server.use(express.json());
+
+server.use('/api/posts', posts);
+server.use('/api/users', users);
+
+
+//endpoints
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+    res.send(`
+      <h2>Lambda WEP API III Challenge :)</h>
+      <p>Blogposts... continued!</p>
+    `);
 });
-
-//custom middleware
-
-function logger(req, res, next) {
-
-};
 
 module.exports = server;
